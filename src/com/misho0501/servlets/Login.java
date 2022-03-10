df1eb545343bd55fb4f36f4126dc664d738aaf53
Mihail Georgiev <mmisho0501@gmail.com>
2022-03-10 13:53:19 +0200

    Lesson 2

---
+++ src/com/misho0501/servlets/Login.java
@@ -0,0 +1,42 @@
+package com.misho0501.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Login", urlPatterns = {"/login"})
+public class Login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+// read form fields
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        System.out.println("username: " + username);
+        System.out.println("password: " + password);
+
+        // do some processing here...
+
+        // get response writer
+        PrintWriter writer = response.getWriter();
+
+        // build HTML code
+        String htmlRespone = "<html>";
+        htmlRespone += "<h2>Your username is: " + username + "<br/>";
+        htmlRespone += "Your password is: " + password + "</h2>";
+        htmlRespone += "</html>";
+
+        // return response
+        writer.println(htmlRespone);
+    }
+}
